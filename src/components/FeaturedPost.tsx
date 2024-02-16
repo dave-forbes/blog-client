5d8b5c97a72cd6a2e3330dacaa991266f3857160
David Forbes <davidforbesi@hotmail.com>
2024-02-16 14:15:51 +0000

    new component created

---
+++ src/components/FeaturedPost.tsx
@@ -0,0 +1,32 @@
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import image from "../assets/jef-willemyns-mluUYXoTotY-unsplash.jpg";
+
+const FeaturedPost = () => {
+  return (
+    <Box pos="relative">
+      <Image src={image} h="500px" w="100%" objectFit="cover"></Image>
+      <Box
+        bg="lightBg"
+        pos="absolute"
+        bottom="0"
+        w="400px"
+        left="100px"
+        p="2rem"
+      >
+        <Heading color="headerText">Climbing in a Changing Climate</Heading>
+        <Text
+          fontSize="xl"
+          fontWeight="500"
+          color="headerText"
+          pl="0.5rem"
+          pt="1rem"
+        >
+          Navigating the Impact of Environmental Shifts on the Climbing
+          Community
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default FeaturedPost;
