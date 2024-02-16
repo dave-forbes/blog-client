dfdb5c56a3251b1adf8eede648bf9115125eb994
David Forbes <davidforbesi@hotmail.com>
2024-02-16 12:23:55 +0000

    adjust layout and add divider and img

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { Button, Flex, Heading, Center, Spacer, Box } from "@chakra-ui/react";
+import { Heading, Box, Image, Text } from "@chakra-ui/react";
+import Header from "./components/Header";
+import image from "./assets/jef-willemyns-mluUYXoTotY-unsplash.jpg";
 
 function App() {
   return (
-    <Box bg="light">
-      <Center h="100vh">
-        <Flex direction="column" gap="2">
-          <Heading as="h1" size="4xl" color="headerText">
-            WELCOME TO MY BLOG!
+    <Box bg="light" h="100vh">
+      <Box maxW="1200px" mx="auto">
+        <Header />
+        <Box mt="4rem" p={4}>
+          <Heading as="h1" size="4xl" color="headerText" w={[400, 500, 600]}>
+            WELCOME TO CLIMBTRIBE.
           </Heading>
-          <Spacer />
-          <Flex gap="4" align="center" justify="center">
-            <Button colorScheme="cyan">Cyan</Button>
-            <Button colorScheme="blackAlpha">BlackAlpha</Button>
-          </Flex>
-        </Flex>
-      </Center>
+          <Text
+            fontSize="xl"
+            fontWeight="500"
+            color="headerText"
+            pl="0.5rem"
+            pt="1rem"
+          >
+            Blog articles about climbing.
+          </Text>
+        </Box>
+        <Box
+          borderTop="1px"
+          borderColor="borderColor"
+          opacity="0.5"
+          m="10px 0"
+        ></Box>
+        <Image src={image}></Image>
+      </Box>
     </Box>
   );
 }
