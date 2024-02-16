1d6be568d5020537cc9a6259dfa76b1e564503b9
David Forbes <davidforbesi@hotmail.com>
2024-02-16 16:10:02 +0000

    added icon and dropdown for smaller devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,83 @@
-import { Box, Flex, Spacer, Link, Button, Heading } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Box,
+  Flex,
+  Button,
+  IconButton,
+  useBreakpointValue,
+  Heading,
+  Link,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import Divider from "./Divider";
 
-const Header = () => {
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showBurgerMenu = useBreakpointValue({ base: true, md: false });
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Box p={4} color="headerText">
-      <Flex alignItems="center">
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      zIndex={999}
+      bg="lightBg"
+      maxW="1200px"
+      mx="auto"
+    >
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        px={6}
+        py={4}
+        color="headerText"
+      >
         <Box>
-          <Link href="#" fontSize="xl" fontWeight="bold">
-            <Heading>climbtribe</Heading>
-          </Link>
+          <Box>
+            <Link href="#" fontSize="xl" fontWeight="bold">
+              <Heading>climbtribe</Heading>
+            </Link>
+          </Box>
         </Box>
-        <Spacer />
-        <Box>
-          <Link href="#" mr={4}>
-            Sign Up
-          </Link>
-          <Button colorScheme="blue">Login</Button>
-        </Box>
+        {showBurgerMenu ? (
+          <IconButton
+            display={{ base: "flex", md: "none" }}
+            onClick={toggleMenu}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label="Toggle navigation"
+            bg="mediumBg"
+          />
+        ) : (
+          <Box>
+            <Link href="#" mr={4}>
+              Sign Up
+            </Link>
+            <Button colorScheme="blue">Login</Button>
+          </Box>
+        )}
+        {/* Render menu items */}
+        {isOpen && (
+          <Flex align="center" w="100%" gap={5} direction="column" m={5}>
+            <Link w="50%" href="#" mr={4} textAlign="center">
+              Sign Up
+            </Link>
+            <Button w="50%" colorScheme="blue">
+              Login
+            </Button>
+          </Flex>
+        )}
       </Flex>
-      <Box borderTop="1px" borderColor="borderColor" opacity="0.5" m={4}></Box>
+      <Divider />
     </Box>
   );
-};
+}
 
 export default Header;
