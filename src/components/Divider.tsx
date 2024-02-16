d689065ebb9445484365f2b6d1611469d65d00cc
David Forbes <davidforbesi@hotmail.com>
2024-02-16 16:12:08 +0000

    create new divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+const Divider = () => {
+  return (
+    <Box borderTop="1px" borderColor="borderColor" opacity="0.5" mx={5}></Box>
+  );
+};
+
+export default Divider;
