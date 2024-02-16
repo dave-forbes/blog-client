2f6caa41aa04fb25d50a41150d3ce1ae0e6abf91
David Forbes <davidforbesi@hotmail.com>
2024-02-16 14:15:40 +0000

    theme colors upated

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,11 @@ const theme = extendTheme({
     heading: "Gilda Display, sans-serif", // Default font for headings
   },
   colors: {
-    light: "rgb(254 253 251)",
+    lightBg: "rgb(254 253 251)",
+    mediumBg: "rgb(250 246 240)",
     headerText: "#2e4456",
     borderColor: "#c9c8c8",
+    lightText: "#686868",
   },
 });
 
