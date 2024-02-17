8d28f7ad8c9a9a7354f6137039818441fdfe9d60
David Forbes <davidforbesi@hotmail.com>
2024-02-17 12:52:13 +0000

    implement JWT decode to determine if user is logged in

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Header from "./components/Header";
 import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
 function App() {
+  const [isloggedIn, setIsLoggedIn] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleResize = () => {
@@ -17,14 +19,46 @@ function App() {
       window.removeEventListener("resize", handleResize);
     };
   }, [windowWidth]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // Decode the token to get its expiration time
+      const decodedToken = jwtDecode<JwtPayload>(token);
+      console.log(decodedToken);
+      if (decodedToken) {
+        const expirationTime = decodedToken.exp;
+        if (expirationTime) {
+          const currentTime = Date.now();
+          console.log(currentTime);
+          const exp = expirationTime * 1000;
+          console.log(exp);
+          if (currentTime < expirationTime * 1000) {
+            setIsLoggedIn(true);
+          } else {
+            // Token expired, clear it from storage and redirect to login
+            localStorage.removeItem("token");
+            setIsLoggedIn(false);
+          }
+        }
+      }
+    }
+  }, []);
+
   return (
     <Box bg="lightBg">
       <Box maxW="1200px" mx="auto" pt="73px">
         <Box as="section" my={10}>
-          <Header />
+          <Header
+            isLoggedIn={isloggedIn}
+            logout={() => {
+              localStorage.removeItem("token");
+              setIsLoggedIn(false);
+            }}
+          />
         </Box>
       </Box>
-      <Outlet />
+      <Outlet context={setIsLoggedIn} />
       <Box as="section" mt={10} bg="mediumBg">
         <Box maxW="1200px" mx="auto">
           <Footer />
