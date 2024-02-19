2e2df1578e11918a6db6240741d7d346bbf57027
David Forbes <davidforbesi@hotmail.com>
2024-02-19 14:07:56 +0000

    added date and author and updated props to receive full post object

--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -1,42 +1,66 @@
 import {
   Box,
+  Flex,
   Heading,
   Image,
   LinkBox,
   LinkOverlay,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import image from "../assets/jef-willemyns-mluUYXoTotY-unsplash.jpg";
+import { PostI } from "../interfaces";
 
-interface FeaturePostProps {
-  title: string;
-  text: string;
-  _id: string;
+interface FeaturedPostProps {
+  post: PostI | null;
 }
 
-const FeaturedPost = ({ title, text, _id }: FeaturePostProps) => {
+const FeaturedPost = ({ post }: FeaturedPostProps) => {
+  if (!post) {
+    return null;
+  }
   const extractFirstSentence = (text: string): string => {
     const sentences = text.replace("Introduction:", "").split(".");
     const firstSentence = sentences[0].trim();
     return firstSentence;
   };
-  const summary = extractFirstSentence(text);
+  const summary = extractFirstSentence(post.text);
+
+  const date = new Date(post.createdAt);
+  const smallScreen = useBreakpointValue({ base: true, md: false });
 
   return (
     <LinkBox pos="relative">
-      <LinkOverlay href={"/posts/" + _id}>
+      <LinkOverlay href={"/posts/" + post._id}>
         <Image src={image} h="500px" w="100%" objectFit="cover"></Image>
         <Box bg="lightBg" pos="absolute" bottom="0" p={5} m={5}>
-          <Heading color="headerText">{title}</Heading>
-          <Text
-            fontSize="xl"
-            fontWeight="500"
-            color="headerText"
-            pl="0.5rem"
-            pt="1rem"
-          >
-            {summary}
-          </Text>
+          <Heading color="headerText">{post.title}</Heading>
+          <Flex align="baseline" gap={5}>
+            <Text
+              fontSize="xl"
+              fontWeight="500"
+              color="headerText"
+              pl="0.5rem"
+              pt="1rem"
+            >
+              {summary + "..."}
+            </Text>
+            {!smallScreen ? (
+              <>
+                <Text>
+                  <strong>{post.user.username}</strong> -{" "}
+                  {date.toLocaleString("en-gb", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </Text>
+              </>
+            ) : (
+              ""
+            )}
+          </Flex>
         </Box>
       </LinkOverlay>
     </LinkBox>
