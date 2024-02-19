3648deafd5a26fc15095496d1aa6c15bd1e404fa
David Forbes <davidforbesi@hotmail.com>
2024-02-19 13:36:48 +0000

    fetched data from params and used to render post

--- src/components/ReadPost.tsx
+++ src/components/ReadPost.tsx
@@ -5,13 +5,50 @@ import {
   Flex,
   Center,
   useBreakpointValue,
+  SkeletonText,
 } from "@chakra-ui/react";
 import image from "../assets/jef-willemyns-mluUYXoTotY-unsplash.jpg";
 import Divider from "./Divider";
 import Comment from "./Comment";
+import { useParams } from "react-router-dom";
+import { PostI } from "../interfaces";
+import { useEffect, useState } from "react";
 
 const ReadPost = () => {
+  const [post, setPost] = useState<PostI>();
+  const [loading, setLoading] = useState(true);
+  const { postId } = useParams();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          `https://blog-api-production-7c83.up.railway.app/posts/read/${postId}`,
+          {
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache",
+            referrerPolicy: "no-referrer",
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        const post = await response.json();
+        post.createdAt = new Date(post.createdAt);
+
+        setPost(post);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        // Handle error
+      }
+    };
+    fetchPosts();
+  }, []);
+
   const smallScreen = useBreakpointValue({ base: true, md: false });
+
   return (
     <>
       <Flex
@@ -34,9 +71,17 @@ const ReadPost = () => {
           gap={5}
         >
           <Flex direction="column" align="center" gap={5}>
-            <Heading color="headerText">Climbing in a Changing Climate</Heading>
+            <Heading color="headerText">{post ? post.title : ""}</Heading>
             <Text>
-              <strong>David Forbes</strong> - Mon 19th Feb 2024
+              <strong>{post ? post.user.username : ""}</strong> -
+              {post
+                ? post.createdAt.toLocaleString("en-gb", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })
+                : ""}
             </Text>
           </Flex>
           {/* <Text fontSize="xl" fontWeight="500" color="headerText">
@@ -45,75 +90,11 @@ const ReadPost = () => {
               Community
             </em>
           </Text> */}
-          <Text fontSize="lg">
-            As climbers, we are intimately connected to the natural world. From
-            scaling majestic rock faces to ascending icy peaks, our pursuits
-            take us to some of the most breathtaking environments on Earth.
-            However, as our planet undergoes profound changes due to climate
-            change, the very landscapes we cherish and rely on are being altered
-            in ways that profoundly affect our climbing experiences. In this
-            blog post, we'll explore how climate change is impacting the
-            climbing community and discuss the importance of collective action
-            to mitigate its effects.
-          </Text>
-          <Text fontSize="lg">
-            One of the most visible effects of climate change on climbing is the
-            rapid retreat of glaciers and the thawing of permafrost in alpine
-            environments. For climbers who venture into these regions, this
-            means encountering unstable terrain, increased rockfall, and altered
-            route conditions. Traditional climbing routes may become more
-            hazardous or even impassable as ice melts and rock stability is
-            compromised. Moreover, the loss of glaciers diminishes the aesthetic
-            beauty and challenge of classic climbs, altering the very essence of
-            these revered routes.
-          </Text>
-          <Text fontSize="lg">
-            Climate change is also causing shifts in weather patterns, leading
-            to changes in climbing seasons. Warmer temperatures and
-            unpredictable weather can shorten climbing windows or make certain
-            areas inaccessible for longer periods. Climbers must adapt to these
-            shifting conditions by adjusting their schedules and seeking out
-            alternative climbing destinations. However, for many climbers,
-            particularly those who rely on specific seasons for expeditions or
-            competitions, these changes pose significant challenges and may
-            require reevaluation of long-term climbing goals.
-          </Text>
-          <Text fontSize="lg">
-            Extreme weather events, such as hurricanes, wildfires, and flash
-            floods, are becoming more frequent and intense due to climate
-            change. These events not only pose immediate dangers to climbers but
-            also cause long-term damage to climbing infrastructure and natural
-            landscapes. Rockfall, erosion, and vegetation loss can alter
-            climbing routes and access trails, requiring extensive maintenance
-            and restoration efforts by the climbing community. Additionally, the
-            threat of natural disasters necessitates heightened awareness and
-            preparedness among climbers, emphasizing the importance of safety
-            protocols and risk management practices.
-          </Text>
-          <Text fontSize="lg">
-            In the face of these challenges, climbers are increasingly
-            recognizing the importance of conservation and advocacy efforts to
-            protect the environments we love to explore. Local climbing
-            organizations, environmental groups, and outdoor brands are joining
-            forces to address climate change through initiatives such as carbon
-            offset programs, sustainable climbing practices, and land
-            conservation projects. By supporting these initiatives and
-            participating in community-led efforts, climbers can contribute to
-            preserving the places where we climb for future generations.
-          </Text>
-          <Text fontSize="lg">
-            The impacts of climate change on the climbing community are profound
-            and multifaceted, affecting everything from route conditions to
-            access to climbing destinations. As stewards of the natural world,
-            climbers have a responsibility to address these challenges through
-            collective action and advocacy. By advocating for climate action,
-            supporting conservation efforts, and practicing sustainable climbing
-            habits, we can work towards preserving the environments that inspire
-            and challenge us. Together, we can ensure that future generations of
-            climbers have the opportunity to experience the beauty and thrill of
-            climbing in a world where the mountains remain resilient and
-            unspoiled.
-          </Text>
+          <SkeletonText isLoaded={!loading} noOfLines={80} spacing="4">
+            <Text fontSize="lg" whiteSpace="pre-wrap">
+              {post ? post.text : ""}
+            </Text>
+          </SkeletonText>
         </Flex>
       </Flex>
       <Flex direction="column" maxW="1200px" mx="auto" gap={5}>
