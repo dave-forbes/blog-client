3f7b84cb23d0e34de355657a61488e81431a42b4
David Forbes <davidforbesi@hotmail.com>
2024-02-21 10:23:23 +0000

    remove unnessary window width state variables and event listeners, refactor token decode for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,36 @@
 import { Box } from "@chakra-ui/react";
 import Header from "./components/Header";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { useAuth } from "./authContext";
 
 function App() {
-  const { isLoggedIn, setIsLoggedIn } = useAuth();
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [windowWidth]);
+  const { setIsLoggedIn } = useAuth();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    // Decode the token to get its expiration time
     if (token) {
-      // Decode the token to get its expiration time
       const decodedToken: { id: string; username: string; exp: number } =
         jwtDecode(token);
-      console.log(decodedToken);
       localStorage.setItem("userId", decodedToken.id);
       localStorage.setItem("userName", decodedToken.username);
-      if (decodedToken) {
-        const expirationTime = decodedToken.exp;
-        if (expirationTime) {
-          const currentTime = Date.now();
-          console.log(currentTime);
-          const exp = expirationTime * 1000;
-          console.log(exp);
-          if (currentTime < expirationTime * 1000) {
-            setIsLoggedIn(true);
-          } else {
-            // Token expired, clear it from storage and redirect to login
-            localStorage.removeItem("token");
-            localStorage.removeItem("userName");
-            localStorage.removeItem("userId");
-            setIsLoggedIn(false);
-          }
-        }
+      const expirationTimeS = decodedToken.exp;
+      const expirationTimeMs = expirationTimeS * 1000;
+      const currentTime = Date.now();
+      if (currentTime < expirationTimeMs) {
+        setIsLoggedIn(true);
+      } else {
+        // Token expired, clear it from storage and redirect to login
+        localStorage.removeItem("token");
+        localStorage.removeItem("userName");
+        localStorage.removeItem("userId");
+        setIsLoggedIn(false);
       }
+    } else {
+      setIsLoggedIn(false);
     }
   }, []);
 
@@ -65,7 +48,7 @@ function App() {
           />
         </Box>
       </Box>
-      <Outlet context={[isLoggedIn, setIsLoggedIn]} />
+      <Outlet />
       <Box as="section" mt={10} bg="mediumBg">
         <Box maxW="1200px" mx="auto">
           <Footer />
