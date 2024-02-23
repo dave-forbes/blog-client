a0bc396f378cce3c9031d1a360379c9a2f6801a3
David Forbes <davidforbesi@hotmail.com>
2024-02-23 11:22:09 +0000

    applied max width and margin to all content

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <Box bg="lightBg">
-      <Box maxW="1200px" mx="auto" pt="73px">
-        <Box as="section" my={10}>
-          <Header />
+      <Box maxW="1200px" mx="auto">
+        <Box pt="73px">
+          <Box as="section" my={10}>
+            <Header />
+          </Box>
         </Box>
-      </Box>
-      <Outlet />
-      <Box as="section" mt={10} bg="mediumBg">
-        <Box maxW="1200px" mx="auto">
-          <Footer />
+        <Outlet />
+        <Box as="section" mt={10} bg="mediumBg">
+          <Box>
+            <Footer />
+          </Box>
         </Box>
       </Box>
     </Box>
