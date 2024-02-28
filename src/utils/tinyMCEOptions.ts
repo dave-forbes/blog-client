d6a62f619318d8a5d59b659995047c313630d5c2
David Forbes <davidforbesi@hotmail.com>
2024-02-28 15:45:24 +0000

    add new util tinyMCE options
    - options for configuring the plugins and ui for tinyMCE, extracted to make my enormous create post component a bit smaller and easier to read

---
+++ src/utils/tinyMCEOptions.ts
@@ -0,0 +1,27 @@
+const tinyMCEOptions = {
+  height: 500,
+  menubar: false,
+  plugins: [
+    "advlist",
+    "autolink",
+    "lists",
+    "link",
+    "image",
+    "charmap",
+    "preview",
+    "anchor",
+    "searchreplace",
+    "visualblocks",
+    "code",
+    "fullscreen",
+    "insertdatetime",
+    "media",
+    "table",
+    "code",
+    "help",
+    "wordcount",
+  ],
+  toolbar: "undo redo | blocks ",
+};
+
+export default tinyMCEOptions;
