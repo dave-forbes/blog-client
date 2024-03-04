42243d768a3fd1cd4e411a1583ce7ed769a77c96
David Forbes <davidforbesi@hotmail.com>
2024-03-04 12:12:41 +0000

    implement delete comment
    - when delete button clicked, open modal, if mistake, cancel to close modal, if confirmed, pass comment to parent to be deleted

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,25 +1,50 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Flex,
+  Text,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from "@chakra-ui/react";
 import { AtSignIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import { CommentI } from "../utils/interfaces";
-import { useEffect, useState } from "react";
+import React from "react";
 
 interface CommentProps {
   comment: CommentI;
   onEdit: (comment: CommentI) => void;
+  onDelete: (comment: CommentI) => void;
 }
 
-const Comment = ({ comment, onEdit }: CommentProps) => {
+const Comment = ({ comment, onEdit, onDelete }: CommentProps) => {
   const [user, setUser] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef(null);
+
   const date = new Date(comment.createdAt);
 
   const handleEdit = () => {
     onEdit(comment);
   };
 
+  const handleDelete = () => {
+    setIsOpen(true);
+  };
+
   useEffect(() => {
     const userId = localStorage.getItem("userId");
     if (userId) setUser(userId);
   });
+
+  const confirmDelete = () => {
+    onDelete(comment);
+    setIsOpen(false);
+  };
 
   return (
     <Flex p={5} direction="column" gap={3}>
@@ -42,7 +67,7 @@ const Comment = ({ comment, onEdit }: CommentProps) => {
         </Flex>
       </Flex>
       <Text>{comment.text}</Text>
-      {user == comment.user._id && (
+      {user === comment.user._id && (
         <Flex justify="flex-end" gap={10}>
           <Button size="xs" colorScheme="teal" onClick={handleEdit}>
             <Flex gap={3} align="center">
@@ -50,12 +75,38 @@ const Comment = ({ comment, onEdit }: CommentProps) => {
               <Text fontSize="xs">Edit</Text>
             </Flex>
           </Button>
-          <Button size="xs" colorScheme="red">
+          <Button size="xs" colorScheme="red" onClick={handleDelete}>
             <Flex gap={3} align="center">
               <DeleteIcon boxSize={4} />
               <Text fontSize="xs">Delete</Text>
             </Flex>
           </Button>
+          <AlertDialog
+            isOpen={isOpen}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+          >
+            <AlertDialogOverlay>
+              <AlertDialogContent>
+                <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                  Delete Comment
+                </AlertDialogHeader>
+
+                <AlertDialogBody>
+                  Are you sure? You can't undo this action afterwards.
+                </AlertDialogBody>
+
+                <AlertDialogFooter>
+                  <Button ref={cancelRef} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme="red" onClick={confirmDelete} ml={3}>
+                    Delete
+                  </Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
         </Flex>
       )}
     </Flex>
